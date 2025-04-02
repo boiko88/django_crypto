8b06bb93754f104d1d775483727f26546f716e22
unknown <bo1ko.ant@yandex.com>
2025-04-02 19:53:36 +0300

    changed urls crypto to main

--- crypto/urls.py
+++ crypto/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CryptoPageView, HomePageView
 
 urlpatterns = [
-    path('', CryptoPageView.as_view(), name='crypto'),  # This will handle the root '/'
-    path('home/', HomePageView.as_view(), name='home'),  # This will handle '/home/'
+    path('', HomePageView.as_view(), name='home'),
+    path('crypto/', CryptoPageView.as_view(), name='crypto'),
 ]
