615aa6568fe554f59e3319f5b7eef3b2eb911e7e
unknown <bo1ko.ant@yandex.com>
2025-04-04 13:41:49 +0300

    added Docker. Due to Redis, it is easier to test the app in Docker

--- django_crypto_news/settings.py
+++ django_crypto_news/settings.py
@@ -131,10 +131,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")  # Uses "localhost" when running locally
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": "redis://redis:6379/1",
     }
 }
 
