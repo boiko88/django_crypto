a40831c07085225911e7eb7003d1db16a3eefa2a
unknown <bo1ko.ant@yandex.com>
2025-04-09 21:19:51 +0300

    added models for Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=256)
+    body_text = models.TextField(max_length=5000)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Blog.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
