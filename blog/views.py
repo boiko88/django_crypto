18c2a7cc8fc5b38baf5d203c57441d45471882f4
unknown <bo1ko.ant@yandex.com>
2025-04-12 12:42:30 +0300

    added search functionality

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,18 @@ class BlogPageView(LoginRequiredMixin, ListView):
     context_object_name = 'blogs'
     ordering = ['-created_at']
     paginate_by = 2
+
+    # Search function
+    def get_queryset(self):
+        queryset = Blog.objects.all()
+        query = self.request.GET.get('q', '')
+
+        if query:
+            queryset = queryset.filter(title__icontains=query) | queryset.filter(body_text__icontains=query)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
