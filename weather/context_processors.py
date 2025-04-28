6b4d6ee1f8f4ba6cadf0fc5c97ad984994203bc5
unknown <bo1ko.ant@yandex.com>
2025-04-28 19:41:09 +0300

    modified weather box to connect real place from user's profile to weather

--- weather/context_processors.py
+++ weather/context_processors.py
@@ -1,13 +1,16 @@
 from django.conf import settings as conf_settings
 from django.core.cache import cache
+
+from users.models import Profile
+from geopy.geocoders import Nominatim
 import requests
 
 
-def fetch_weather_api():
-    ''' Function to fetch data from the weather API. '''
+def fetch_weather_api(lat, lon):
+    ''' Fetch weather data for given latitude and longitude. '''
     MAIN_URL = 'http://api.openweathermap.org/data/2.5/weather?'
     API_KEY = conf_settings.WEATHER_KEY
-    CITY = 'lat=55.75&lon=37.61'  # Moscow
+    CITY = f'lat={lat}&lon={lon}'
 
     url = MAIN_URL + CITY + '&units=metric&appid=' + API_KEY
     response = requests.get(url).json()
@@ -26,16 +29,21 @@ def fetch_weather_api():
 
 
 def weather_data(request):
-    ''' Checks cache before making an API request. '''
-    cached_weather = cache.get('weather_data')
+    # Default: Moscow
+    lat, lon = 55.75, 37.61
 
-    if cached_weather:
-        return {'weather': cached_weather}  # Return cached data if available
+    if request.user.is_authenticated:
+        try:
+            profile = Profile.objects.get(user=request.user)
+            if profile.latitude and profile.longitude:
+                lat, lon = profile.latitude, profile.longitude
+            elif profile.address:
+                geolocator = Nominatim(user_agent="django_weather_app")
+                location = geolocator.geocode(profile.address)
+                if location:
+                    lat, lon = location.latitude, location.longitude
+        except Profile.DoesNotExist:
+            pass
 
-    # Otherwise, fetch new data from API
-    new_weather_data = fetch_weather_api()
-
-    if new_weather_data:
-        cache.set('weather_data', new_weather_data, timeout=300)  # Cache for 5 minutes
-
+    new_weather_data = fetch_weather_api(lat, lon)
     return {'weather': new_weather_data}
