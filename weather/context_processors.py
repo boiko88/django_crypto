912f4b7922e31d0d59a520caff05d89f7a695849
unknown <bo1ko.ant@yandex.com>
2025-05-13 10:19:30 +0300

    modified context processors for weather to let users change location

--- weather/context_processors.py
+++ weather/context_processors.py
@@ -6,7 +6,7 @@ from geopy.geocoders import Nominatim
 import requests
 
 
-def fetch_weather_api(lat, lon):
+def fetch_weather_api(lat, lon) -> dict | None:
     ''' Fetch weather data for given latitude and longitude. '''
     MAIN_URL = 'http://api.openweathermap.org/data/2.5/weather?'
     API_KEY = conf_settings.WEATHER_KEY
@@ -28,20 +28,26 @@ def fetch_weather_api(lat, lon):
     return None  # In case of an error
 
 
-def weather_data(request):
-    # Default: Moscow
-    lat, lon = 55.75, 37.61
+def weather_data(request) -> dict:
+    # Default: Singapore
+    lat, lon = 1.3521, 103.8198  # Singapore coordinates
 
     if request.user.is_authenticated:
         try:
             profile = Profile.objects.get(user=request.user)
+            # First check if user has manually set coordinates
             if profile.latitude and profile.longitude:
                 lat, lon = profile.latitude, profile.longitude
+            # If no coordinates but has address, try to geocode it
             elif profile.address:
                 geolocator = Nominatim(user_agent="django_weather_app")
                 location = geolocator.geocode(profile.address)
                 if location:
                     lat, lon = location.latitude, location.longitude
+                    # Update the profile with the geocoded coordinates
+                    profile.latitude = lat
+                    profile.longitude = lon
+                    profile.save()
         except Profile.DoesNotExist:
             pass
 
